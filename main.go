2f3fcfcf603af4c1edbabe01eae342aba1754e6c
David Docampo <david@docampo.ch>
2023-01-22 23:03:14 +0100

    refactor main file to use cobra

--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/SayHeyD/sops-age-manager/internal/key"
-	"log"
+	"github.com/SayHeyD/sops-age-manager/cmd"
 )
 
 func main() {
-	systemTrayIconFlagSet := false
-	systemTrayIconFlagName := "tray-icon"
-
-	systemTrayIcon := flag.Bool(systemTrayIconFlagName, false, "Add a tray icon for managing keys with a UI")
-	activeKey := flag.String("key", "", "The key to select for sops")
-
-	flag.Parse()
-
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == systemTrayIconFlagName {
-			systemTrayIconFlagSet = true
-		}
-	})
-
-	if systemTrayIconFlagSet {
-		fmt.Println(*systemTrayIcon)
-	}
-
-	if *activeKey == "" {
-		log.Fatal("No key name provided")
-	}
-
-	keys := key.GetAvailableKeys("")
-
-	for _, ageKey := range keys {
-		if ageKey.Name == *activeKey {
-			ageKey.SetActive()
-			fmt.Println(fmt.Sprintf("Set \"%s\" as active key", ageKey.Name))
-			return
-		}
-	}
-
-	log.Fatal(fmt.Sprintf("No key with name \"%s\" found", *activeKey))
+	cmd.Execute()
 }
