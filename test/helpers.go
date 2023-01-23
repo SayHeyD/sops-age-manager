8cb9880c684ad8ead5ed71de6937fbcdaeb43946
David Docampo <david@docampo.ch>
2023-01-23 23:59:17 +0100

    remove redundant path seperator

--- test/helpers.go
+++ test/helpers.go
@@ -9,7 +9,7 @@ import (
 // getTestBaseDir returns the base path for creating test directories and
 // creates the base directory if it does not exist
 func getTestBaseDir(t *testing.T) string {
-	dir := os.TempDir() + string(os.PathSeparator) + "sops-age-manager"
+	dir := os.TempDir() + "sops-age-manager"
 
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		if err := os.Mkdir(dir, os.ModePerm); err != nil {
