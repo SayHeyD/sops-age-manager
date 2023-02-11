63372cbbe6943d2f5daf08d8520689268e3b281f
David Docampo <david@docampo.ch>
2023-02-11 23:23:45 +0100

    add confirmation step before private keys are printed out in console

--- cmd/key_list.go
+++ cmd/key_list.go
@@ -1,14 +1,18 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/SayHeyD/sops-age-manager/pkg/key"
 	"github.com/spf13/cobra"
+	"os"
+	"strings"
 )
 
 var (
-	listKeyWithPrivateKeys = false
-	listKeyWithFileName    = false
+	listKeyWithPrivateKeys     = false
+	listKeyWithFileName        = false
+	skipPrivateKeyConfirmation = false
 
 	listKeyCommand = &cobra.Command{
 		Use:   "list",
@@ -22,12 +26,38 @@ Filepath and private key can be displayed optionally`,
 )
 
 func init() {
-	listKeyCommand.Flags().BoolVarP(&listKeyWithPrivateKeys, "private-key", "p", false, "displays the private-key")
-	listKeyCommand.Flags().BoolVarP(&listKeyWithFileName, "file", "f", false, "displays the filepath of the key")
+	listKeyCommand.Flags().BoolVarP(&listKeyWithPrivateKeys, "private-key", "p", false,
+		"displays the private-key")
+	listKeyCommand.Flags().BoolVarP(&listKeyWithFileName, "file", "f", false,
+		"displays the filepath of the key")
+	listKeyCommand.Flags().BoolVarP(&skipPrivateKeyConfirmation, "yes", "y", false,
+		"automatically confirm that you want private keys to be shown")
 }
 
 func listKeys() {
 	keys := key.GetAvailableKeys("")
+
+	if listKeyWithPrivateKeys {
+		if !skipPrivateKeyConfirmation {
+			fmt.Printf("Please confirm that you want to display the pricate keys (yes/y): ")
+
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("could not retrieve user input: ", err)
+				return
+			}
+
+			input = strings.TrimSuffix(input, "\n")
+
+			normalizedUserInput := strings.ToLower(input)
+
+			if normalizedUserInput != "yes" && normalizedUserInput != "y" {
+				fmt.Println("Question not answered with \"yes\" or \"y\". Aborting ...")
+				return
+			}
+		}
+	}
 
 	for _, ageKey := range keys {
 		fmt.Println()
