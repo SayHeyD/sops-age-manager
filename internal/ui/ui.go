85d6becef80ac513a44968d487cef96c324604e4
David Docampo <david@docampo.ch>
2024-01-27 12:09:19 +0100

    hide dock app on macos

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -8,7 +8,25 @@ import (
 	"github.com/SayHeyD/sops-age-manager/pkg/key"
 	"github.com/atotto/clipboard"
 	"log"
+	"time"
 )
+
+/** Hide macos dock application when app is started
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Cocoa
+#import <Cocoa/Cocoa.h>
+
+int
+SetActivationPolicy(void) {
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
+    return 0;
+}
+*/
+import "C"
+
+func setActivationPolicy() {
+	C.SetActivationPolicy()
+}
 
 func Init(config *config.Config) {
 	a := app.New()
@@ -81,6 +99,13 @@ func Init(config *config.Config) {
 		m := fyne.NewMenu("SAM", keyMenu)
 		desk.SetSystemTrayMenu(m)
 	}
+
+	a.Lifecycle().SetOnStarted(func() {
+		go func() {
+			time.Sleep(200 * time.Millisecond)
+			setActivationPolicy()
+		}()
+	})
 
 	a.Run()
 }
